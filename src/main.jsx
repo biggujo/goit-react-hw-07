e7e44697b233503c3139689e85c506e4af7512a1
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-04-11 20:33:34 +0300

    Move to Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/App/App';
 import 'modern-normalize';
 import { Provider } from 'react-redux';
+import App from './components/App/App.jsx';
 import { store } from './redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
