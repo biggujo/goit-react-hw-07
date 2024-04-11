8d7fb1708fd1d7fd96e174bb97060eea72a81dff
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-04-11 22:16:17 +0300

    Add "No contacts found" after filter search

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -7,6 +7,10 @@ import {
 export default function ContactList() {
   const filteredContacts = useSelector(selectFilteredContacts);
 
+  if (filteredContacts.length === 0) {
+    return <p>No contacts found.</p>;
+  }
+
   return (<ul>
     {filteredContacts.map(({
       id,
